6b73b92322efbb7e13d43fa8b32e7b6508f758f9
Volker Mische <volker.mische@gmail.com>
2023-09-12 14:06:29 +0200

    feat: add binary for Synthetic PoRep challenge generation
    Also fix the original PoRep challenge generation.

--- fil-proofs-bin/src/bin/challenges-synth.rs
+++ fil-proofs-bin/src/bin/challenges-synth.rs
@@ -1,18 +1,19 @@
 use anyhow::Result;
+use blstrs::Scalar as Fr;
+use ff::PrimeField;
 use fil_proofs_bin::cli;
-use filecoin_hashers::sha256::Sha256Domain;
 use log::info;
 use serde::{Deserialize, Serialize};
 use serde_hex::{SerHex, StrictPfx};
 use storage_proofs_core::util::NODE_SIZE;
-use storage_proofs_porep::stacked::LayerChallenges;
+use storage_proofs_porep::stacked::SynthChallenges;
 
 #[derive(Debug, Deserialize, Serialize)]
-struct ChallengesParameters {
+struct ChallengesSynthParameters {
+    #[serde(with = "SerHex::<StrictPfx>")]
+    comm_r: [u8; 32],
     /// The number of challenges to create.
     num_challenges: usize,
-    /// Which partition to create the challenges for.
-    partition: u8,
     #[serde(with = "SerHex::<StrictPfx>")]
     replica_id: [u8; 32],
     /// Sector size is used to calculate the number of nodes.
@@ -22,30 +23,26 @@ struct ChallengesParameters {
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct ChallengesOutput {
+struct ChallengesSynthOutput {
     challenges: Vec<usize>,
 }
 
 fn main() -> Result<()> {
     fil_logger::maybe_init();
 
-    let params: ChallengesParameters = cli::parse_stdin()?;
+    let params: ChallengesSynthParameters = cli::parse_stdin()?;
     info!("{:?}", params);
 
-    let leaves = usize::try_from(params.sector_size)
+    let sector_nodes = usize::try_from(params.sector_size)
         .expect("sector size must be smaller than the default integer size on this platform")
         / NODE_SIZE;
-    let layer_challenges = LayerChallenges::new(params.num_challenges);
-    let challenges = layer_challenges.derive::<Sha256Domain>(
-        leaves,
-        &params.replica_id.into(),
-        // For normal PoRep this value isn't used, so we can put in an arbitrary value.
-        &[1u8; 32].into(),
-        &params.seed,
-        params.partition,
-    );
+    let replica_id = Fr::from_repr_vartime(params.replica_id).expect("must be valid field element");
+    let comm_r = Fr::from_repr_vartime(params.comm_r).expect("must be valid field element");
+    let challenges =
+        SynthChallenges::new(sector_nodes, &replica_id, &comm_r, params.num_challenges)
+            .gen_porep_challenges(params.num_challenges, &params.seed);
 
-    let output = ChallengesOutput { challenges };
+    let output = ChallengesSynthOutput { challenges };
     info!("{:?}", output);
     cli::print_stdout(output)?;
 
